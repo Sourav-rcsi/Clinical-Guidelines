c2d400bdd0a40c1297b6cd0038031144f9cdeef7
Sourav-rcsi <167217706+Sourav-rcsi@users.noreply.github.com>
2024-05-14 19:54:42 +0100

    Add files via upload
    Output Data

---
+++ op_data.py
@@ -0,0 +1,96 @@
+def output_data():
+    data = {
+        'Q1': {
+            'WSR': {
+                'WSR-SvG': {
+                    'statistic': 3.0,
+                    'p-value': 0.01825586058745926
+                },
+                'WSR-SvC': {
+                    'statistic': 1.5,
+                    'p-value': 0.1974660733580187
+                },
+                'WSR-SvT': {
+                    'statistic': 4.0,
+                    'p-value': 0.33399825582199794
+                }
+            },
+            'MWU': {
+                'MWU-SvG': {
+                    'statistic': 202.5,
+                    'p-value': 0.01691760024866596
+                },
+                'MWU-SvC': {
+                    'statistic': 162.0,
+                    'p-value': 0.37789247403276127
+                },
+                'MWU-SvT': {
+                    'statistic': 161.0,
+                    'p-value': 0.4068298269729832
+                }
+            }
+        },
+        'Q2': {
+            'WSR': {
+                'WSR-SvG': {
+                    'statistic': 13.0,
+                    'p-value': 0.03829066625474006
+                },
+                'WSR-SvC': {
+                    'statistic': 5.0,
+                    'p-value': 0.4795001221869535
+                },
+                'WSR-SvT': {
+                    'statistic': 6.0,
+                    'p-value': 0.6802795473344503
+                }
+            },
+            'MWU': {
+                'MWU-SvG': {
+                    'statistic': 214.0,
+                    'p-value': 0.007500348593184498
+                },
+                'MWU-SvC': {
+                    'statistic': 160.5,
+                    'p-value': 0.4725987107953924
+                },
+                'MWU-SvT': {
+                    'statistic': 149.0,
+                    'p-value': 0.8452478900473642
+                }
+            }
+        },
+        'Q3': {
+            'WSR': {
+                'WSR-SvG': {
+                    'statistic': 0.0,
+                    'p-value': 0.006655605482949377
+                },
+                'WSR-SvC': {
+                    'statistic': 0.0,
+                    'p-value': 0.10247043485974937
+                },
+                'WSR-SvT': {
+                    'statistic': 6.0,
+                    'p-value': 0.6802795473344503
+                }
+            },
+            'MWU': {
+                'MWU-SvG': {
+                    'statistic': 211.5,
+                    'p-value': 0.005399171877712661
+                },
+                'MWU-SvC': {
+                    'statistic': 171.5,
+                    'p-value': 0.19611268210266952
+                },
+                'MWU-SvT': {
+                    'statistic': 153.0,
+                    'p-value': 0.654479818870475
+                }
+            }
+        }
+    }
+    return data
+
+
